fd81353001c1b23f0409e783d7cc0cc4bba41e24
Williams Calado <williamscalado@gmail.com>
2022-03-08 17:50:34 -0300

    implementacion of Interface for City and Customers

---
+++ src/Interface/cityInterface/index.ts
@@ -0,0 +1,7 @@
+export interface Icity {
+    _id?: string,
+    name: string,
+    countryId: string,
+    active: boolean,
+    createAt: number
+}
---
+++ src/Interface/customerInterface/index.ts
@@ -0,0 +1,10 @@
+export interface Icustomer{
+    _id?: string,
+    name: string,
+    lastName: string,
+    gender: string,
+    birthdayDate: number,
+    age: string,
+    idCity: string,
+    createAt?: number
+}
